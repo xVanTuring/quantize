ca62b91677992881b11cd98b820a1aae652a897c
xVan Turing <LXF990922@outlook.com>
2018-05-22 19:36:21 +0800

    add count to palette

--- quantize.js
+++ quantize.js
@@ -12,26 +12,26 @@
  */
 if (!pv) {
     var pv = {
-        map: function(array, f) {
+        map: function (array, f) {
             var o = {};
-            return f ? array.map(function(d, i) {
+            return f ? array.map(function (d, i) {
                 o.index = i;
                 return f.call(o, d);
             }) : array.slice();
         },
-        naturalOrder: function(a, b) {
+        naturalOrder: function (a, b) {
             return (a < b) ? -1 : ((a > b) ? 1 : 0);
         },
-        sum: function(array, f) {
+        sum: function (array, f) {
             var o = {};
-            return array.reduce(f ? function(p, d, i) {
+            return array.reduce(f ? function (p, d, i) {
                 o.index = i;
                 return p + f.call(o, d);
-            } : function(p, d) {
+            } : function (p, d) {
                 return p + d;
             }, 0);
         },
-        max: function(array, f) {
+        max: function (array, f) {
             return Math.max.apply(null, f ? pv.map(array, f) : array);
         }
     }
@@ -59,7 +59,7 @@ var newPixels = myPixels.map(function(p) {
 });
  
  */
-var MMCQ = (function() {
+var MMCQ = (function () {
     // private constants
     var sigbits = 5,
         rshift = 8 - sigbits,
@@ -84,26 +84,26 @@ var MMCQ = (function() {
         }
 
         return {
-            push: function(o) {
+            push: function (o) {
                 contents.push(o);
                 sorted = false;
             },
-            peek: function(index) {
+            peek: function (index) {
                 if (!sorted) sort();
                 if (index === undefined) index = contents.length - 1;
                 return contents[index];
             },
-            pop: function() {
+            pop: function () {
                 if (!sorted) sort();
                 return contents.pop();
             },
-            size: function() {
+            size: function () {
                 return contents.length;
             },
-            map: function(f) {
+            map: function (f) {
                 return contents.map(f);
             },
-            debug: function() {
+            debug: function () {
                 if (!sorted) sort();
                 return contents;
             }
@@ -123,14 +123,14 @@ var MMCQ = (function() {
         vbox.histo = histo;
     }
     VBox.prototype = {
-        volume: function(force) {
+        volume: function (force) {
             var vbox = this;
             if (!vbox._volume || force) {
                 vbox._volume = ((vbox.r2 - vbox.r1 + 1) * (vbox.g2 - vbox.g1 + 1) * (vbox.b2 - vbox.b1 + 1));
             }
             return vbox._volume;
         },
-        count: function(force) {
+        count: function (force) {
             var vbox = this,
                 histo = vbox.histo;
             if (!vbox._count_set || force) {
@@ -149,11 +149,11 @@ var MMCQ = (function() {
             }
             return vbox._count;
         },
-        copy: function() {
+        copy: function () {
             var vbox = this;
             return new VBox(vbox.r1, vbox.r2, vbox.g1, vbox.g2, vbox.b1, vbox.b2, vbox.histo);
         },
-        avg: function(force) {
+        avg: function (force) {
             var vbox = this,
                 histo = vbox.histo;
             if (!vbox._avg || force) {
@@ -177,15 +177,15 @@ var MMCQ = (function() {
                     }
                 }
                 if (ntot) {
-                    vbox._avg = [~~(rsum / ntot), ~~ (gsum / ntot), ~~ (bsum / ntot)];
+                    vbox._avg = [~~(rsum / ntot), ~~(gsum / ntot), ~~(bsum / ntot)];
                 } else {
                     //console.log('empty box');
-                    vbox._avg = [~~(mult * (vbox.r1 + vbox.r2 + 1) / 2), ~~ (mult * (vbox.g1 + vbox.g2 + 1) / 2), ~~ (mult * (vbox.b1 + vbox.b2 + 1) / 2)];
+                    vbox._avg = [~~(mult * (vbox.r1 + vbox.r2 + 1) / 2), ~~(mult * (vbox.g1 + vbox.g2 + 1) / 2), ~~(mult * (vbox.b1 + vbox.b2 + 1) / 2)];
                 }
             }
             return vbox._avg;
         },
-        contains: function(pixel) {
+        contains: function (pixel) {
             var vbox = this,
                 rval = pixel[0] >> rshift;
             gval = pixel[1] >> rshift;
@@ -199,7 +199,7 @@ var MMCQ = (function() {
     // Color map
 
     function CMap() {
-        this.vboxes = new PQueue(function(a, b) {
+        this.vboxes = new PQueue(function (a, b) {
             return pv.naturalOrder(
                 a.vbox.count() * a.vbox.volume(),
                 b.vbox.count() * b.vbox.volume()
@@ -207,21 +207,24 @@ var MMCQ = (function() {
         });;
     }
     CMap.prototype = {
-        push: function(vbox) {
+        push: function (vbox) {
             this.vboxes.push({
                 vbox: vbox,
                 color: vbox.avg()
             });
         },
-        palette: function() {
-            return this.vboxes.map(function(vb) {
-                return vb.color
+        palette: function () {
+            return this.vboxes.map(function (vb) {
+                return {
+                    color: vb.color,
+                    count: vb.vbox.count()
+                }
             });
         },
-        size: function() {
+        size: function () {
             return this.vboxes.size();
         },
-        map: function(color) {
+        map: function (color) {
             var vboxes = this.vboxes;
             for (var i = 0; i < vboxes.size(); i++) {
                 if (vboxes.peek(i).vbox.contains(color)) {
@@ -230,7 +233,7 @@ var MMCQ = (function() {
             }
             return this.nearest(color);
         },
-        nearest: function(color) {
+        nearest: function (color) {
             var vboxes = this.vboxes,
                 d1, d2, pColor;
             for (var i = 0; i < vboxes.size(); i++) {
@@ -246,10 +249,10 @@ var MMCQ = (function() {
             }
             return pColor;
         },
-        forcebw: function() {
+        forcebw: function () {
             // XXX: won't  work yet
             var vboxes = this.vboxes;
-            vboxes.sort(function(a, b) {
+            vboxes.sort(function (a, b) {
                 return pv.naturalOrder(pv.sum(a.color), pv.sum(b.color))
             });
 
@@ -273,7 +276,7 @@ var MMCQ = (function() {
         var histosize = 1 << (3 * sigbits),
             histo = new Array(histosize),
             index, rval, gval, bval;
-        pixels.forEach(function(pixel) {
+        pixels.forEach(function (pixel) {
             rval = pixel[0] >> rshift;
             gval = pixel[1] >> rshift;
             bval = pixel[2] >> rshift;
@@ -292,7 +295,7 @@ var MMCQ = (function() {
             bmax = 0,
             rval, gval, bval;
         // find min/max
-        pixels.forEach(function(pixel) {
+        pixels.forEach(function (pixel) {
             rval = pixel[0] >> rshift;
             gval = pixel[1] >> rshift;
             bval = pixel[2] >> rshift;
@@ -359,7 +362,7 @@ var MMCQ = (function() {
                 partialsum[i] = total;
             }
         }
-        partialsum.forEach(function(d, i) {
+        partialsum.forEach(function (d, i) {
             lookaheadsum[i] = total - d
         });
 
@@ -374,8 +377,8 @@ var MMCQ = (function() {
                     left = i - vbox[dim1];
                     right = vbox[dim2] - i;
                     if (left <= right)
-                        d2 = Math.min(vbox[dim2] - 1, ~~ (i + right / 2));
-                    else d2 = Math.max(vbox[dim1], ~~ (i - 1 - left / 2));
+                        d2 = Math.min(vbox[dim2] - 1, ~~(i + right / 2));
+                    else d2 = Math.max(vbox[dim1], ~~(i - 1 - left / 2));
                     // avoid 0-count boxes
                     while (!partialsum[d2]) d2++;
                     count2 = lookaheadsum[d2];
@@ -392,7 +395,7 @@ var MMCQ = (function() {
         // determine the cut planes
         return maxw == rw ? doCut('r') :
             maxw == gw ? doCut('g') :
-            doCut('b');
+                doCut('b');
     }
 
     function quantize(pixels, maxcolors) {
@@ -409,7 +412,7 @@ var MMCQ = (function() {
 
         // check that we aren't below maxcolors already
         var nColors = 0;
-        histo.forEach(function() {
+        histo.forEach(function () {
             nColors++
         });
         if (nColors <= maxcolors) {
@@ -418,7 +421,7 @@ var MMCQ = (function() {
 
         // get the beginning vbox from the colors
         var vbox = vboxFromPixels(pixels, histo),
-            pq = new PQueue(function(a, b) {
+            pq = new PQueue(function (a, b) {
                 return pv.naturalOrder(a.count(), b.count())
             });
         pq.push(vbox);
@@ -463,7 +466,7 @@ var MMCQ = (function() {
         // console.log(pq.size(), pq.debug().length, pq.debug().slice());
 
         // Re-sort by the product of pixel occupancy times the size in color space.
-        var pq2 = new PQueue(function(a, b) {
+        var pq2 = new PQueue(function (a, b) {
             return pv.naturalOrder(a.count() * a.volume(), b.count() * b.volume())
         });
         while (pq.size()) {
